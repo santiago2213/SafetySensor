982598f331f8e11e5c1e4e988fa8a83a2bc2e639
santiago2213 <112100026+santiago2213@users.noreply.github.com>
2023-06-12 20:23:57 -0400

    Add files via upload

---
+++ RecordedDataTest.cs
@@ -0,0 +1,202 @@
+﻿using Moq;
+using LibraryGroup7;
+using MvcGroup7.Controllers;
+using MvcGroup7.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.Intrinsics;
+using MvcGroup7.ViewModels;
+
+namespace TestGroup7
+{
+    public class RecordedDataTest
+    {
+        [Fact]
+        public void ShouldSearchForRecordedDataByTemperatureandDate()
+        {
+            Mock<IRecordedDataRepo> mockRecordedDataRepo = new Mock<IRecordedDataRepo>();
+
+            Mock<IFacilityRepo> mockIFacilityRepo = new Mock<IFacilityRepo>();
+
+            Mock<IAppUserRepo> mockIAppUserRepo = new Mock<IAppUserRepo>(); 
+
+            List<RecordedData> mockRecordedData = CreateMockData();
+
+            mockRecordedDataRepo.Setup(m => m.GetAllRecordedData()).Returns(mockRecordedData);
+            List<Facility> mockFacility = CreateMockFacility();
+            mockIFacilityRepo.Setup(a => a.GetAllFacillities()).Returns(mockFacility);
+
+            int expectedNumberOfRecordedData = 1;
+
+            RecordedDataController recordedDataController = new RecordedDataController(mockRecordedDataRepo.Object, mockIFacilityRepo.Object, mockIAppUserRepo.Object);
+
+            // int? inputSearchTemperature = 95;
+            //int? inputSearchPressure = 20;
+            // ViewResult viewResult = recordedDataController.SearchRecordedData(inputSearchTemperature, inputSearchPressure) as ViewResult; // "=" assignment operator
+
+            SearchRecordedDataViewModel viewModel = new SearchRecordedDataViewModel
+            {
+                SearchDataType = "Temperature",
+                SearchDataValue = 90,
+                
+                SearchRecordedDataDate = new DateTime(2022, 7, 10)
+
+            };
+
+            ViewResult viewResult = recordedDataController.SearchRecordedData(viewModel) as ViewResult;
+            SearchRecordedDataViewModel resultModel = viewResult.Model as SearchRecordedDataViewModel;
+
+            List<RecordedData> actualResultList = resultModel.SearchResult;
+
+            int actualNumberOfRecordedData = actualResultList.Count;
+
+            Assert.Equal(expectedNumberOfRecordedData, actualNumberOfRecordedData);
+        }
+
+        [Fact]
+        public void ShouldSearchForRecordedDataByTemperature()
+        {
+            Mock<IRecordedDataRepo> mockRecordedDataRepo = new Mock<IRecordedDataRepo>();
+            Mock<IFacilityRepo> mockIFacilityRepo = new Mock<IFacilityRepo>();
+            Mock<IAppUserRepo> mockIAppUserRepo = new Mock<IAppUserRepo>();
+            List<RecordedData> mockRecordedData = CreateMockData();
+            mockRecordedDataRepo.Setup(m => m.GetAllRecordedData()).Returns(mockRecordedData);
+            List<Facility> mockFacility = CreateMockFacility();
+            mockIFacilityRepo.Setup(a => a.GetAllFacillities()).Returns(mockFacility);
+
+            int expectedNumberOfRecordedData = 1;
+
+            RecordedDataController recordedDataController = new RecordedDataController(mockRecordedDataRepo.Object, mockIFacilityRepo.Object, mockIAppUserRepo.Object);
+
+            SearchRecordedDataViewModel viewModel = new SearchRecordedDataViewModel
+            {
+                SearchDataType = "Temperature",
+                SearchDataValue = 90,
+                SearchRecordedDataDate = null
+              
+
+            };
+
+            ViewResult viewResult = recordedDataController.SearchRecordedData(viewModel) as ViewResult;
+            SearchRecordedDataViewModel resultModel = viewResult.Model as SearchRecordedDataViewModel;
+
+            List<RecordedData> actualResultList = resultModel.SearchResult;
+
+            int actualNumberOfRecordedData = actualResultList.Count;
+
+            Assert.Equal(expectedNumberOfRecordedData, actualNumberOfRecordedData);
+        }
+
+        [Fact]
+        public void ShouldSearchForRecordedDataByPressure()
+        {
+            Mock<IRecordedDataRepo> mockRecordedDataRepo = new Mock<IRecordedDataRepo>();
+            Mock<IFacilityRepo> mockIFacilityRepo = new Mock<IFacilityRepo>();
+            Mock<IAppUserRepo> mockIAppUserRepo = new Mock<IAppUserRepo>();
+            List<RecordedData> mockRecordedData = CreateMockData();
+            mockRecordedDataRepo.Setup(m => m.GetAllRecordedData()).Returns(mockRecordedData);
+            List<Facility> mockFacility = CreateMockFacility();
+            mockIFacilityRepo.Setup(a => a.GetAllFacillities()).Returns(mockFacility);
+            int expectedNumberOfRecordedData = 3;
+
+            RecordedDataController recordedDataController = new RecordedDataController(mockRecordedDataRepo.Object, mockIFacilityRepo.Object, mockIAppUserRepo.Object);
+
+            SearchRecordedDataViewModel viewModel = new SearchRecordedDataViewModel
+            {
+                SearchDataType = "Pressure",
+                SearchDataValue = null,
+                SearchRecordedDataDate = null,
+               FacilityID = null
+                
+               
+
+
+            };
+
+            ViewResult viewResult = recordedDataController.SearchRecordedData(viewModel) as ViewResult;
+            SearchRecordedDataViewModel resultModel = viewResult.Model as SearchRecordedDataViewModel;
+
+            List<RecordedData> actualResultList = resultModel.SearchResult;
+
+            int actualNumberOfRecordedData = actualResultList.Count;
+
+            Assert.Equal(expectedNumberOfRecordedData, actualNumberOfRecordedData);
+
+
+        }
+
+
+
+        [Fact]
+        public void ShouldSearchForRecordedDateByTime()
+        {
+            Mock<IRecordedDataRepo> mockRecordedDataRepo = new Mock<IRecordedDataRepo>();
+            Mock<IFacilityRepo> mockIFacilityRepo = new Mock<IFacilityRepo>();
+            Mock<IAppUserRepo> mockIAppUserRepo = new Mock<IAppUserRepo>();
+            List<Facility> mockFacility = CreateMockFacility();
+            mockIFacilityRepo.Setup(a => a.GetAllFacillities()).Returns(mockFacility);
+
+            List<RecordedData> mockRecordedData = CreateMockData();
+            mockRecordedDataRepo.Setup(m => m.GetAllRecordedData()).Returns(mockRecordedData);
+
+            int expectedNumberOfRecordedData = 2;
+
+            RecordedDataController recordedDataController = new RecordedDataController(mockRecordedDataRepo.Object, mockIFacilityRepo.Object, mockIAppUserRepo.Object);
+
+            SearchRecordedDataViewModel viewModel = new SearchRecordedDataViewModel
+            {
+                SearchDataType = null,
+                SearchDataValue = null,
+                SearchRecordedDataDate = new DateTime(2019, 7, 9)
+
+            };
+
+            ViewResult viewResult = recordedDataController.SearchRecordedData(viewModel) as ViewResult;
+            SearchRecordedDataViewModel resultModel = viewResult.Model as SearchRecordedDataViewModel;
+
+            List<RecordedData> actualResultList = resultModel.SearchResult;
+
+            int actualNumberOfRecordedData = actualResultList.Count;
+
+            Assert.Equal(expectedNumberOfRecordedData, actualNumberOfRecordedData);
+        }
+
+        public List<Facility> CreateMockFacility()
+        {
+            List<Facility> mockFacility = new List<Facility>();
+            Facility facility = new Facility { FacilityID = 1, FacilityName= "Test1" };
+            mockFacility.Add(facility);
+            facility = new Facility { FacilityID = 2, FacilityName = "Test2" };
+            mockFacility.Add(facility);
+
+            return mockFacility;
+        }
+   
+
+        public List<RecordedData> CreateMockData()
+        {
+            List<RecordedData> mockData = new List<RecordedData>();
+            RecordedData recordedData = new RecordedData { RecordedDataID = 1,RecordedDataType = "Temperature", RecordedDataValue = 90, RecordedDataDate = new DateTime(2022, 7, 10) };
+            mockData.Add(recordedData);
+
+            recordedData = new RecordedData { RecordedDataID = 2, RecordedDataType = "Temperature", RecordedDataValue = 98, RecordedDataDate = new DateTime(2022, 8, 15) };
+            mockData.Add(recordedData);
+
+            recordedData = new RecordedData { RecordedDataID = 3, RecordedDataType = "Temperature" , RecordedDataValue = 101, RecordedDataDate = new DateTime(2019, 7, 9) };
+            mockData.Add(recordedData);
+
+            recordedData = new RecordedData { RecordedDataID = 4, RecordedDataType = "Pressure", RecordedDataValue = 20, RecordedDataDate = new DateTime(2019, 7, 9) };
+            mockData.Add(recordedData);
+
+            recordedData = new RecordedData { RecordedDataID = 5, RecordedDataType = "Pressure", RecordedDataValue = 25, RecordedDataDate = new DateTime(2019, 5, 17) };
+            mockData.Add(recordedData);
+
+            recordedData = new RecordedData { RecordedDataID = 6, RecordedDataType = "Pressure", RecordedDataValue = 15, RecordedDataDate = new DateTime(2019, 1, 19) };
+            mockData.Add(recordedData);
+
+            return mockData;
+        }
+
+
+
+    }
+}
---
+++ SensorRepairRequestTest.cs
@@ -0,0 +1,140 @@
+﻿using LibraryGroup7;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using MvcGroup7.Controllers;
+using MvcGroup7.Models;
+using MvcGroup7.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGroup7
+{
+    public class SensorRepairRequestTest
+    {
+        [Fact]
+        public void ShouldSearchForCompletedSensorRepairRequestsByLocation()
+        {
+            Mock<ISensorRepairRequestRepo> mockSensorRepairRequestRepo = new Mock<ISensorRepairRequestRepo>();
+            Mock<IAppUserRepo> mockAppUserRepo = new Mock<IAppUserRepo>();
+
+            List<SensorRepairRequest> mockSensorRepairRequestData = CreateMockData();
+            mockSensorRepairRequestRepo.Setup(m => m.GetAllSensorRepairRequests()).Returns(mockSensorRepairRequestData);
+
+            int expectedNumberOfServiceRequests = 1;
+
+            SensorRepairRequestController sensorRepairRequestController = new SensorRepairRequestController(mockSensorRepairRequestRepo.Object, mockAppUserRepo.Object);
+
+            string inputSearchLocation = "Lab 1";
+            SearchSensorRepairRequestsViewModel viewModel = new SearchSensorRepairRequestsViewModel { SearchDate = null, SearchEngineerFullName = null };
+
+            ViewResult viewResult = sensorRepairRequestController.SearchSensorRepairRequest(viewModel) as ViewResult;
+
+            List<SensorRepairRequest> actualResultList = viewResult.Model as List<SensorRepairRequest>;
+
+            int actualNumberOfServiceRequests = actualResultList.Count;
+
+            Assert.Equal(expectedNumberOfServiceRequests, actualNumberOfServiceRequests);
+        }
+
+        [Fact]
+        public void ShouldSearchForRepairRequestWithNotes()
+        {
+            Mock<ISensorRepairRequestRepo> mockSensorRepairRequestRepo = new Mock<ISensorRepairRequestRepo>();
+            Mock<IAppUserRepo> mockAppUserRepo = new Mock<IAppUserRepo>();
+
+            List<SensorRepairRequest> mockSensorRepairRequestData = CreateMockData();
+            mockSensorRepairRequestRepo.Setup(m => m.GetAllSensorRepairRequests()).Returns(mockSensorRepairRequestData);
+
+            int expectedNumberOfReapirRequests = 1;
+
+            SensorRepairRequestController sensorRepairRequestController = new SensorRepairRequestController(mockSensorRepairRequestRepo.Object, mockAppUserRepo.Object);
+
+            string inputITFullName = "IT 1";
+            SearchSensorRepairRequestsViewModel viewModel = new SearchSensorRepairRequestsViewModel { SearchDate = null, SearchEngineerFullName = null };
+
+            ViewResult viewResult = sensorRepairRequestController.SearchSensorRepairRequest(viewModel) as ViewResult;
+
+            List<SensorRepairRequest> actualResultList = viewResult.Model as List<SensorRepairRequest>;
+
+            int actualNumberOfServiceRequests = actualResultList.Count;
+
+            Assert.Equal(expectedNumberOfReapirRequests, actualNumberOfServiceRequests);
+
+
+        }
+
+
+
+
+
+
+
+
+
+        public List<SensorRepairRequest> CreateMockData()
+        {
+            List<SensorRepairRequest> mockData = new List<SensorRepairRequest>();
+
+            List<Notes> mockNotes = new List<Notes>();
+            string description = "Check fuse";
+            string locationName = "Lab 1";
+            string engineerName = "Engineer 1";
+            DateTime dateSensorRepairRequested = new DateTime(2022, 10, 31);
+            string supervisorName = "Supervisor 1";
+            DateTime dateDecided = new DateTime(2022, 10, 31);
+            string itName = "IT 1";
+            DateTime dateSensorRepaired = new DateTime(2022, 11, 1);
+            SensorLocation sensorLocation = new SensorLocation { LocationName = locationName};
+            Engineer engineer = new Engineer { Fullname = engineerName};
+            IT it = new IT { Fullname = itName};
+            Supervisor supervisor = new Supervisor { Fullname = supervisorName };
+            SensorRepairRequest sensorRepairRequest = new SensorRepairRequest { DateSensorRepairRequested = dateSensorRepairRequested, DateSensorRepaired = dateSensorRepaired, Engineer = engineer, IT = it, Supervisor = supervisor, SensorLocation = sensorLocation };
+
+            Notes notes = new Notes{SensorRepairRequest = sensorRepairRequest, Description="Needs new fuse", NoteCreated = new DateTime(2022, 11, 1), IT=it };
+            mockNotes.Add(notes);
+
+            notes = new Notes{SensorRepairRequest = sensorRepairRequest, Description = "Change fuse", NoteCreated = new DateTime(2022, 11, 2), Supervisor = supervisor };
+            mockNotes.Add(notes);
+
+            sensorRepairRequest.SensorRepairRequestNotes = mockNotes;
+
+            mockData.Add(sensorRepairRequest);
+
+
+            description = "Check wires";
+            locationName = "Lab 1";
+            engineerName = "Engineer 1";
+            dateSensorRepairRequested = new DateTime(2022, 11, 3);
+            supervisorName = "Supervisor 1";
+            dateDecided = new DateTime(2022, 11, 4);
+            sensorLocation = new SensorLocation { LocationName = locationName };
+            engineer = new Engineer { Fullname = engineerName };
+            it = new IT { Fullname = itName };
+            supervisor = new Supervisor { Fullname = supervisorName };
+            sensorRepairRequest = new SensorRepairRequest { DateSensorRepairRequested = dateSensorRepairRequested, Engineer = engineer, Supervisor = supervisor, SensorLocation = sensorLocation };
+            mockData.Add(sensorRepairRequest);
+
+            description = "Replace Sensor";
+            locationName = "Lab 2";
+            engineerName = "Engineer 2";
+            dateSensorRepairRequested = new DateTime(2022, 11, 5);
+            supervisorName = "Supervisor 2";
+            itName = "IT 1";
+            dateDecided = new DateTime(2022, 11, 6);
+            sensorLocation = new SensorLocation { LocationName = locationName };
+            engineer = new Engineer { Fullname = engineerName };
+            it = new IT { Fullname = itName };
+            supervisor = new Supervisor { Fullname = supervisorName };
+            sensorRepairRequest = new SensorRepairRequest { DateSensorRepairRequested = dateSensorRepairRequested, Engineer = engineer, Supervisor = supervisor, IT = it, SensorLocation = sensorLocation };
+            mockData.Add(sensorRepairRequest);
+
+
+            return mockData;
+
+        }
+
+    }
+}
